e2ca634f3bde17c38b964ec8980415ec9ef1797f
Estefania Simbana <esimbanam3@est.ups.edu.ec>
2026-05-14 01:36:58 -0500

    Commit inicial

---
+++ script.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------
+#Script de prueba para Python
+#Autor:Estefania Simbaña <esimbanam3@est.ups.edu.ec>
+#Fecha: 27/04/2026
+#-------------------------------------------------
+
+def main():
+    nombre = input ("Cual es tu nombre: ")
+    print("Hola", nombre,"Bienvenido a mi mundo!")
+    
+if __name__=="__main__":
+    main()
